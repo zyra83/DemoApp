bea0fd0489ec2d9ad8a57060e9139fc6c372271b
MKonzali <41302010+MKonzali@users.noreply.github.com>
2024-02-15 20:05:19 +0100

    Update DemoApplication.java

--- DemoApplication.java
+++ DemoApplication.java
@@ -5,15 +5,27 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
+    @Value("${country:France}")
+    private String country;
+
+    @Value("${city:Paris}")
+    private String city;
+    
     @GetMapping("/")
-    public String home() {
-        return "Hello, Kubernetes!";
+    public String home(@RequestParam(value = "name", defaultValue = "Kubernetes") String name) {
+        return String.format("Hello, %s!", name);
+    }
+
+    @GetMapping("/")
+    public String details(@RequestParam(value = "name", defaultValue = "Kubernetes") String name) {
+        return String.format("Hello, %s!", name);
     }
 
     public static void main(String[] args) {
