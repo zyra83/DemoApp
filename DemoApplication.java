4ae39992c66b0e64325fa7ebc7fe69f9a1a0f87c
MKonzali <41302010+MKonzali@users.noreply.github.com>
2024-02-15 23:51:02 +0100

    Update DemoApplication.java

--- DemoApplication.java
+++ DemoApplication.java
@@ -20,7 +20,38 @@ public class DemoApplication {
 
     @Value("${city:Red}")
     private String color;
-    
+  
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String helloName(@PathVariable String name) {
+        return String.format("Hello, %s!", name);
+    }
+
+    @PostMapping("/hello")
+    public String helloPost(@RequestBody String name) {
+        return String.format("Hello, %s!", name);
+    }
+
+    @GetMapping("/country")
+    public String country() {
+        return String.format("Country: %s", country);
+    }
+
+    @GetMapping("/city")
+    public String city() {
+        return String.format("City: %s", city);
+    }
+
+    @GetMapping("/color")
+    public String color() {
+        return String.format("Color: %s", color);
+    }
+
+
     @GetMapping("/")
     public String home(@RequestParam(value = "name", defaultValue = "Kubernetes") String name) {
         return String.format("Hello, %s!", name);
@@ -28,7 +59,7 @@ public class DemoApplication {
 
     @GetMapping("/details")
     public String details() {
-        return String.format("Country: %s \nCity: %s \Color: %s", country,city,color);
+        return String.format("Country: %s \nCity: %s \nColor: %s", country,city,color);
     }
 
     public static void main(String[] args) {
